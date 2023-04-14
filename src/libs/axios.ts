56aee1a224e3da2f0b57bf6d9d6ebe5052653f5f
katsuya6152 <katsuya.6152@gmail.com>
2023-04-15 00:48:38 +0900

    fix axios

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,12 +1,5 @@
 import axios from 'axios'
 
-const resolvedServer = (() => {
-  if (process.env.Server) {
-    return process.env.Server
-  }
-  return 'http://localhost:3001'
-})()
-
 export const ax = axios.create({
-  baseURL: resolvedServer,
+  baseURL: 'https://dashboard-be.onrender.com',
 })
